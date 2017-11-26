09561ab96697aa32b4286abbbca5831423843653
陳美妤\MICO <s3a432090@student.ncut.edu.tw>
2017-11-26 16:31:50 +0800

    設定 Post.php的 fillable屬性

--- app/Post.php
+++ app/Post.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected$table='posts';
+
+    protected$fillable=[
+        'title',
+        'content',
+        'is_feature'
+    ];
 }
